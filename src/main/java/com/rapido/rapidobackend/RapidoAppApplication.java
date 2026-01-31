0e99c44d0e42064aae8b082a42aca1198b0a1746
shubhcode92 <abc123@gmail.com>
2026-02-01 02:12:01 +0530

    Initial commit

---
+++ src/main/java/com/rapido/rapidobackend/RapidoAppApplication.java
@@ -0,0 +1,13 @@
+package com.rapido.rapidobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapidoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rapido/rapidobackend/RapidoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapido.rapidobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
